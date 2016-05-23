7d23029675eb0034888ec67d94edcf490a79bf10
davicosk8 <david_Gz1234@hotmail.com>
2016-05-23 11:40:52 -0400

    ofertas por categoria

--- getofertasporcategoriaid.php
+++ getofertasporcategoriaid.php
@@ -1,19 +1,27 @@
 <?php
 include 'config.php';
-
-$sql = "select * from tbl_ofertas m where m.tbl_categoria_id=:id";
-
+$sql = "select distinct	o.id as ofertaid, 
+				o.nombre as ofertanombre, 
+		        o.nombre_imagen as ofertaimagen, 
+		        date_format(o.fecha_fin,'%W %d of %b - %Y') as ofertafechafin,
+		        e.nombre as empresanombre,
+		        e.id as empresaid from tbl_ofertas o
+		join tbl_ofertas_tbl_sucursales os on o.id=os.tbl_oferta_id
+		join tbl_sucursales s on os.tbl_sucursale_id=s.id
+		join tbl_empresas e on s.tbl_empresa_id=e.id
+		where o.estado_activa=1 and o.tbl_categoria_id= :idc
+		order by o.id desc 
+		limit 15 offset 0";
 try {
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+		$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$stmt = $dbh->prepare($sql);  
-	$stmt->bindParam('id', $_GET['id']);
+	$stmt = $dbh->prepare($sql);
+	$stmt->bindParam('idc', $_GET['idc']);
 	$stmt->execute();
-	$ofertasporid = $stmt->fetchObject();  
+	$ofertas = $stmt->fetchAll(PDO::FETCH_OBJ);
 	$dbh = null;
-	echo '{"item":'. json_encode($ofertasporid) .'}'; 
+	echo '{"items":'. json_encode($ofertas) .'}';
 } catch(PDOException $e) {
-	echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	echo '{"error":{"text":'. $e->getMessage() .'}}';
 }
-
 ?>
